4ae9a8648ea721ee78efcf44acce7d7a6eb8fe0c
itc-s23006 <git@github.com:itc-s23006/WebExtention1.git>
2023-12-06 12:45:01 +0900

    教科書P265の途中まで

---
+++ src/chapter11_2/PredExample.java
@@ -0,0 +1,14 @@
+package chapter11_2;
+
+public class PredExample {
+    public static void main(String[] args) {
+
+        boolean result =/* 1 */
+
+        System.out.print(result);
+    }
+
+    public static boolean pred(int number, Predicate<Integer> pr){
+        /* 2 */
+    }
+}
