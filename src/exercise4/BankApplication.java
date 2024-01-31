d43c6ba17dfdfb8fb5317c5429bce775a3533390
itc-s23006 <git@github.com:itc-s23006/WebExtention1.git>
2024-01-31 12:45:12 +0900

    1/31 OXゲームの作成(完成)と、銀行のプログラムの作成(パート1)。

---
+++ src/exercise4/BankApplication.java
@@ -0,0 +1,28 @@
+package exercise4;
+
+public class BankApplication {
+    public static void main(String[] args) {
+        /*
+        お名前とIDを入力してください。
+        a) 残高照会
+        b) 預入
+        c) 引出
+        d) 取引履歴
+        e) 終了
+         */
+    }
+}
+
+class BankAccount{
+    /* フィールド
+    * customerName 顧客名
+    * customerId 顧客ID
+    * balance 残高
+
+    * メソッド
+    * deposit 預入
+    * withdraw 引き出し
+    * getPreTrans 取引履歴
+    * menu メニュー 操作を選ばせる。 例:bが選ばれたらdepositメソッドを呼びだす。
+     */
+}
